9675057be70954423f46d843f96b9ebedd40580c
Slowstar0010 <starphj007@daum.net>
2023-01-28 21:22:31 +0900

    pip install pyinstaller

--- arma3_TFAR_setup/team_down.py
+++ arma3_TFAR_setup/team_down.py
@@ -6,8 +6,8 @@ def DownloadFile(teamspeak):
     installer = open(file, 'wb')
     installer.close()
     return 
-def install():
+#def install():
+
+
     
-
-
 DownloadFile("https://cdn.discordapp.com/attachments/1011478821247143967/1068808347861647380/TeamSpeak3-Client-win64-3.5.6.exe")
