971ed083c332a7c9fc852944ff28817fec555281
Slowstar0010 <starphj007@daum.net>
2023-01-28 19:17:59 +0900

    test

---
+++ arma3_TFAR_setup/team_down.py
@@ -0,0 +1,3 @@
+from urllib.request import urlretrieve
+
+urlretrieve("https://cdn.discordapp.com/attachments/1011478821247143967/1068808347861647380/TeamSpeak3-Client-win64-3.5.6.exe", ".\TeamSpeak")
